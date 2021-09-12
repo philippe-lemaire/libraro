8416c28c641073bd70f4482f0aeb2e1f4a3a84cc
Philippe Lemaire <philippe.lemaire@protonmail.com>
2021-09-12 19:55:05 +0200

    new structure almost done, lacking models

--- libraro.py
+++ libraro.py
@@ -1,26 +1,18 @@
+import os
+from .libraro import create_app, db
+from .libraro.models import User, Role, Book
+from flask_migrate import Migrate
+
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
-import os
+
 from flask_sqlalchemy import SQLAlchemy
 
 
-app = Flask(__name__)
-Bootstrap(app)
-
-# SQL Setup with sqlite
-basedir = os.path.abspath(os.path.dirname(__file__))
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(
-    basedir, "data/data.sqlite"
-)
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db = SQLAlchemy(app)
+app = create_app(os.getenv("FLASK_CONFIG") or "default")
+migrate = Migrate(app, db)
 
 
-@app.route("/")
-def index():
-    return render_template("index.html")
-
-
-@app.route("/add_a_book")
-def add_a_book():
-    return render_template("add_a_book.html")
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User, Role=Role)
