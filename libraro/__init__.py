06b8da91bbad0759269388bf41fbab99807899c4
Philippe Lemaire <philippe.lemaire@protonmail.com>
2021-09-12 19:11:15 +0200

    new app factory

--- libraro/__init__.py
+++ libraro/__init__.py
@@ -1,8 +1,34 @@
+import os
 from os.path import isfile
 from os.path import dirname
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
+from config import config
 
-version_file = '{}/version.txt'.format(dirname(__file__))
+
+version_file = "{}/version.txt".format(dirname(__file__))
 
 if isfile(version_file):
     with open(version_file) as version_file:
         __version__ = version_file.read().strip()
+
+
+bootstrap = Bootstrap()
+moment = Moment()
+db = SQLAlchemy()
+
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+
+    bootstrap.init_app(app)
+    moment.init_app(app)
+    db.init_app(app)
+
+    # attach routes and custom error pages here
+
+    return app
