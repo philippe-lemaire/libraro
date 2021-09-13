e2777d6df9709329404b0f2a74892a59af143c32
Philippe Lemaire <philippe.lemaire@protonmail.com>
2021-09-13 16:11:27 +0200

    added dummy register view

--- app/auth/views.py
+++ app/auth/views.py
@@ -5,3 +5,8 @@ from flask import render_template
 @auth.route("/login")
 def login():
     return render_template("auth/login.html")
+
+
+@auth.route("/register")
+def register():
+    return render_template("auth/register.html")
