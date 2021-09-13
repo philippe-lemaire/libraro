7889f7f590d653600d14e8435c3bf5a63d1dc5f6
Philippe Lemaire <philippe.lemaire@protonmail.com>
2021-09-13 16:16:20 +0200

    added flask login

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from config import config
 
 bootstrap = Bootstrap()
@@ -17,6 +18,7 @@ def create_app(config_name):
     bootstrap.init_app(app)
     moment.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
 
     # attach routes and custom error pages here
     from .main import main as main_blueprint
