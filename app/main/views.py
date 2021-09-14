dfd7931db6cf4750bd7eb037daae58502f43d8e8
Philippe Lemaire <philippe.lemaire@protonmail.com>
2021-09-14 15:29:21 +0200

    add a book in two successive forms added.

--- app/main/views.py
+++ app/main/views.py
@@ -1,13 +1,14 @@
-from datetime import datetime
-from flask import render_template, session, redirect, url_for
-from flask_login import login_required
+from datetime import datetime, date
+from operator import methodcaller
+from isbnlib import meta
+from flask import render_template, session, redirect, url_for, flash, request
+from flask_login import login_required, current_user
 from . import main
+from app.models import User, Book
+from .forms import IsbnForm, BookForm
 
-
-# import forms
 from .. import db
-
-from ..models import User
+from .. import config
 
 
 @main.route("/")
@@ -15,7 +16,39 @@ def index():
     return render_template("index.html")
 
 
-@main.route("/add_a_book")
+@main.route("/add_a_book", methods=["GET", "POST"])
 @login_required
 def add_a_book():
-    return render_template("add_a_book.html")
+    form = IsbnForm()
+    bookform = BookForm()
+    book = Book()
+    searched = False
+    if form.submit1.data and form.validate():
+        searched = True
+        isbn = form.isbn13.data
+        service = config.get("SERVICE") or "goob"
+        book_response = meta(isbn, service=service)
+        book.isbn13 = book_response["ISBN-13"]
+        book.user_id = current_user.id
+        book.title = book_response["Title"]
+        book.authors = ", ".join(book_response["Authors"])
+        book.year = book_response["Year"]
+        book.last_updated = datetime.now()
+        book.read = False
+        # fill in the book form
+        bookform.isbn13.data = book.isbn13
+        bookform.title.data = book.title
+        bookform.authors.data = book.authors
+        bookform.year.data = book.year
+    if bookform.submit2.data and bookform.validate():
+        book.title = bookform.title.data
+        book.authors = bookform.authors.data
+        book.year = bookform.year.data
+        book.read = bookform.read.data
+        db.session.add(book)
+        db.session.commit()
+        flash(f"{book.title} by {book.authors} has been added to your Libraro.")
+        return redirect(url_for("main.index"))
+    return render_template(
+        "add_a_book.html", form=form, bookform=bookform, searched=searched
+    )
