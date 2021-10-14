f0ea6cedb1bd1d93ec8d49611f962ace0c46e934
Philippe Lemaire <philippe.lemaire@protonmail.com>
2021-10-14 15:38:17 +0200

    for SSL on heroku

--- app/__init__.py
+++ app/__init__.py
@@ -16,7 +16,10 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
+    if app.config["SSL_REDIRECT"]:
+        from flask_sslify import SSLify
 
+        sslify = SSLify(app)
     bootstrap.init_app(app)
     moment.init_app(app)
     db.init_app(app)
