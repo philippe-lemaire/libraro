3557c46a8bc81e25b93ebfaa0e60a16801b4c9c8
Philippe Lemaire <phil@plemaire.net>
2025-07-06 15:31:08 +0200

    book model added to admin

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Book
+
 
 # Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    search_fields = ["title"]
+    list_display = ["isbn", "title", "authors", "publisher"]
+    list_filter = [
+        "authors",
+        "publisher",
+    ]
+
+
+admin.site.register(Book, BookAdmin)
