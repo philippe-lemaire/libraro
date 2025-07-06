4d01f688151f12e2314c602159922275feab45aa
Philippe Lemaire <phil@plemaire.net>
2025-07-06 15:28:12 +0200

    first version of book model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Book(models.Model):
+    isbn = models.CharField(max_length=17, unique=True)
+    title = models.CharField(max_length=400)
+    authors = models.CharField(max_length=400)
+    publisher = models.CharField(max_length=200, blank=True, null=True)
+    cover = models.URLField(blank=True, null=True, max_length=400)
