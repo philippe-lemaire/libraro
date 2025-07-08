497f0d8a3dc119ce665cbd64e19fdcf4330e8a37
Philippe Lemaire <phil@plemaire.net>
2025-07-08 18:17:44 +0200

    book detail view

--- books/urls.py
+++ books/urls.py
@@ -6,4 +6,6 @@ app_name = "books"
 urlpatterns = [
     path("", views.BooksIndexView.as_view(), name="index"),
     path("ajouter", views.add_a_book, name="add_a_book"),
+    path("ajouter-a-la-main", views.manually_add_a_book, name="manually_add_a_book"),
+    path("livre/<int:pk>/", views.BooksDetailView.as_view(), name="book_detail"),
 ]
--- books/views.py
+++ books/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.db import IntegrityError
 from .models import Book
-from .forms import IsbnForm
+from .forms import IsbnForm, BookForm
 from isbnlib import *
 
 
@@ -40,6 +40,10 @@ def add_a_book(request):
             # make keys lowercase
             data = {k.lower(): v for k, v in data.items()}
             print(data)
+            if not data:
+                messages.warning(request, "Livre non trouvé dans les bases ISBN.")
+                return HttpResponseRedirect(reverse("books:add_a_book"))
+
             try:
                 new_book = Book.objects.create(**data)
                 messages.success(request, f"Livre {new_book.title} a été ajouté.")
@@ -48,4 +52,17 @@ def add_a_book(request):
                     request, f"Livre déjà présent dans la base de données."
                 )
             return HttpResponseRedirect(reverse("books:add_a_book"))
+    return render(request, template_name, context)
+
+
+def manually_add_a_book(request):
+    form = BookForm(request.POST or None)
+    context = {"form": form}
+    template_name = "books/manually_add_a_book.html"
+    if request.method == "POST":
+        if form.is_valid():
+            new_book = form.save()
+            messages.success(request, f"Livre {new_book.title} a été ajouté.")
+            return HttpResponseRedirect(reverse("books:manually_add_a_book"))
+
     return render(request, template_name, context)
